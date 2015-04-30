4ff183eaa70b758f3b793f7083e4bd04383143cf
Arthur <arthur@corenzan.com>
2015-04-30 20:48:46 -0300

    try casting the value with toString, returns nothing otherwise

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ var yaml = require('yamljs');
 var dot = require('dot-component');
 
 module.exports = function(src, path) {
-  return dot.get(yaml.parse(src), path);
+  var value = dot.get(yaml.parse(src), path);
+  return value && value.toString ? value.toString() : '';
 };
